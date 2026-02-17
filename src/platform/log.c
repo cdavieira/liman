39f6b3fa1c9d72cd929bc634e9a4771f008d0f20
cdavieira <cd_vieira@hotmail.com>
2026-02-17 09:44:40 -0300

    :sparkles: add timestamp to logging

--- src/platform/log.c
+++ src/platform/log.c
@@ -1,11 +1,33 @@
 #include "platform/log.h"
 #include <stdio.h>
+#include <time.h>
+
+#define BUFFER_SIZE 1024
+
+static char timestamp[BUFFER_SIZE];
+
+static void update_timestamp(void) {
+  time_t t = time(NULL);
+  if (t == -1) {
+    return;
+  }
+
+  struct tm *local = localtime(&t);
+  if (local == NULL) {
+    return;
+  }
+
+  if (strftime(timestamp, BUFFER_SIZE, "[%Y-%m-%d %H:%M:%S]", local) == 0) {
+    return;
+  }
+}
 
 void log_info(const char *fmt, ...) {
   va_list va;
   va_start(va, fmt);
 
-  fprintf(stderr, "info: ");
+  update_timestamp();
+  fprintf(stderr, "%s info: ", timestamp);
   vfprintf(stderr, fmt, va);
   putc('\n', stderr);
 
@@ -16,7 +38,8 @@ void log_debug(const char *fmt, ...) {
   va_list va;
   va_start(va, fmt);
 
-  fprintf(stderr, "debug: ");
+  update_timestamp();
+  fprintf(stderr, "%s debug: ", timestamp);
   vfprintf(stderr, fmt, va);
   putc('\n', stderr);
 
@@ -27,7 +50,8 @@ void log_warning(const char *fmt, ...) {
   va_list va;
   va_start(va, fmt);
 
-  fprintf(stderr, "warning: ");
+  update_timestamp();
+  fprintf(stderr, "%s warning: ", timestamp);
   vfprintf(stderr, fmt, va);
   putc('\n', stderr);
 
@@ -38,7 +62,8 @@ void log_error(const char *fmt, ...) {
   va_list va;
   va_start(va, fmt);
 
-  fprintf(stderr, "error: ");
+  update_timestamp();
+  fprintf(stderr, "%s error: ", timestamp);
   vfprintf(stderr, fmt, va);
   putc('\n', stderr);
 
